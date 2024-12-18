762feb0c764558c427fe383f9f7cd1fd859108e7
Desmilda <dedesayss8@gmail.com>
2024-12-18 11:36:51 +0700

    Add files via upload

---
+++ Latihan1b.php
@@ -0,0 +1,22 @@
+<?php
+
+$angka = 10;
+echo "aku adalah angka $angka . </br>";
+
+//Operasi Perkalian
+$angka *= 8;
+echo "jika aku di kali 8, jumlahku sekarang $angka . </br>";
+
+//Operasi Pembagian
+$angka /= 4;
+echo "jika aku di bagi 4, jumlahku sekarang $angka . </br>";
+
+//operasi Pengurangan
+$angka -= 6;
+echo "jika aku di kurang 6, jumlahku sekarang $angka . </br>";
+
+//Operasi Penjumlahan
+$angka += 2;
+echo "jika aku di tambah 2, jumlahku sekarang $angka";
+
+?>
---
+++ rumuspersegipanjang.php
@@ -0,0 +1,14 @@
+<?php
+
+function luaspersegipanjang($panjang, $lebar) {
+$luas = $panjang * $lebar;
+return $luas;
+}
+
+$panjang = 70;
+$lebar = 30;
+
+$hasilluas = luaspersegipanjang($panjang, $lebar);
+
+echo "luas persegi panjang dengan panjang $panjang dan lebar $lebar adalah: $hasilluas";
+?>
